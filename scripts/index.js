141f74006b85e179ada42519927e9a412ab7aaa2
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-11-04 16:09:30 +0700

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+// Contoh data yang di kelola
+const contacts = [
+    { id: 1, name: 'Adam', telp: '621278121' },
+    { id: 2, name: 'Ade', telp: '620192992' },
+    { id: 3, name: 'Figo', telp: '628908093' },
+];
+
+// Pada saat menambahkan data, id tidak boleh sama
+// Agar id tidak sama, gunakan Date.now()  -  contohnya gini: Date.now()
+
+const addNewContact = () => {
+    // write your code here
+}
+
+const deleteDataById = (id) => {
+    // write your code here, info: menghapus data berdasarkan id yang dikirim di param
+}
